d0c46cb8b7eafd264af63bb34d144e2f185fb46a
opus49 <puskar49@gmail.com>
2023-05-12 14:25:18 -0500

    Add project files.

---
+++ CalloutInterfaceHelper/Functions.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace CalloutInterfaceHelper
+{
+    public static class Functions
+    {
+        public static readonly bool isCalloutInterfaceAvailable;
+
+        static Functions()
+        {
+            var assembly = LSPD_First_Response.Mod.API.Functions.GetAllUserPlugins().FirstOrDefault(x => x.Na)
+        }
+    }
+}
---
+++ CalloutInterfaceHelper/RegisteredCallout.cs
@@ -0,0 +1,21 @@
+﻿using LSPD_First_Response.Mod.Callouts;
+
+namespace CalloutInterfaceHelper
+{
+    public class RegisteredCallout : CalloutInfoAttribute
+    {
+        public RegisteredCallout(string name, CalloutProbability probability, string description, string priority = "", string agency = "")
+            : base(name, probability)
+        {
+            this.Agency = agency;
+            this.Description = description;
+            this.Priority = priority;
+        }
+
+        public string Agency { get; private set; }
+
+        public string Description { get; private set; }
+
+        public string Priority { get; private set; }
+    }
+}
