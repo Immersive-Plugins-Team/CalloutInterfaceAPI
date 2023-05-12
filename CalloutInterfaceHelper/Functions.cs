38ce5604866d790ad86bb4915e42e9f0bf401379
opus49 <puskar49@gmail.com>
2023-05-12 14:37:39 -0500

    initial

--- CalloutInterfaceHelper/Functions.cs
+++ CalloutInterfaceHelper/Functions.cs
@@ -8,7 +8,26 @@ namespace CalloutInterfaceHelper
 
         static Functions()
         {
-            var assembly = LSPD_First_Response.Mod.API.Functions.GetAllUserPlugins().FirstOrDefault(x => x.Na)
+            isCalloutInterfaceAvailable = LSPD_First_Response.Mod.API.Functions.GetAllUserPlugins()
+                .Any(x => x.GetName().Name.Equals("CalloutInterface"));
+        }
+
+        public static void SendCalloutDetails(LSPD_First_Response.Mod.Callouts.Callout callout, string priority, string agency)
+        {
+            if (isCalloutInterfaceAvailable)
+            {
+                CalloutInterface.API.Functions.SendCalloutDetails(callout, priority, agency);
+            }
+        }
+
+        public static void SendMessage(LSPD_First_Response.Mod.Callouts.Callout callout, string message)
+        {
+            if (!isCalloutInterfaceAvailable)
+            {
+                return;
+            }
+
+            CalloutInterface.API.Functions.SendMessage(callout, message);
         }
     }
 }
