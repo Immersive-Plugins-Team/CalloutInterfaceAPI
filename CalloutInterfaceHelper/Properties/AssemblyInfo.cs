281b4f22f9e43be992a4b0872ef981aa1edba61d
opus49 <puskar49@gmail.com>
2023-05-13 09:50:29 -0500

    updated

--- CalloutInterfaceHelper/Properties/AssemblyInfo.cs
+++ CalloutInterfaceHelper/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CalloutInterfaceHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2023 Immersive Plugins Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
